b6b036fa21daf8608cb12268951f3549a65853c8
Vadim Danyliuk <sos19997@gmail.com>
2025-07-03 22:40:23 +0300

    Update task-2.js

--- js/task-2.js
+++ js/task-2.js
@@ -4,25 +4,22 @@ class Storage {
   constructor(initialItems) {
     this.#items = [...initialItems];
   }
-
   getItems() {
     return this.#items;
   }
-
   addItem(newItem) {
     this.#items.push(newItem);
   }
-
   removeItem(itemToRemove) {
     this.#items = this.#items.filter(item => item !== itemToRemove);
   }
 }
 
-const storage = new Storage(['Nanitoids', 'Prolonger', 'Antigravitator']);
+const storage = new Storage(["Nanitoids", "Prolonger", "Antigravitator"]);
+console.log(storage.getItems()); 
+storage.addItem("Droid");
 console.log(storage.getItems());
-storage.addItem('Droid');
-console.log(storage.getItems());
-storage.removeItem('Prolonger');
-console.log(storage.getItems());
-storage.removeItem('Scaner');
+storage.removeItem("Prolonger");
+console.log(storage.getItems()); 
+storage.removeItem("Scaner");
 console.log(storage.getItems());
